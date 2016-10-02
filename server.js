3101eccf7a539addcfcfb31049c0325b631b83d6
Joseph Livengood <joeylivengood@gmail.com>
2016-10-02 14:15:21 +0000

    MongoDB integ/Adding new urls

--- server.js
+++ server.js
@@ -3,10 +3,30 @@
 var express = require('express');
 var app = express();
 var path = require("path");
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/urlshortener';
 
 app.get('/new/:url', function (req, res) {
   var input = req.params.url;
-  res.json(input);
+  var doc = {
+    url: input
+  }
+  mongo.connect(url, function(err, db) {
+    if (err) throw err;
+    var collection = db.collection('url');
+    collection.insert(doc, function(err, data) {
+      if (err) throw err;
+      //console.log(input+doc._id);
+      console.log(JSON.stringify(doc));
+      //res.json(doc);
+      res.json({
+        'orginalurl': doc.url,
+        'shortened': 'URL/'+doc._id
+      })
+      db.close;
+    })
+  });
+  //res.json(input); //res.send()
 });
 
 app.listen(process.env.PORT, function () {
