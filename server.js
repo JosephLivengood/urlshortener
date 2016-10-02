64c477f5ff43b0428b6b135204e72b5f691861ed
Joseph Livengood <joeylivengood@gmail.com>
2016-10-02 13:12:52 +0000

    Cleaned up server.js

--- server.js
+++ server.js
@@ -3,24 +3,10 @@
 var express = require('express');
 var app = express();
 var path = require("path");
-var html_dir = './html/';
-
-app.use(express.static(path.join(__dirname, 'html')));
-
-app.get('/:i', function (req, res) {
-    //get url, redirect
-});
-
-app.get('/index', function (req, res) {
-  res.sendfile(html_dir + 'index.html');
-});
 
 app.get('/new/:url', function (req, res) {
-  var input = decodeURI(req.params.url);
-  var defaultresult = {"actual": null, "short": null};
-  var result = defaultresult;
-
-  res.json(result);
+  var input = req.params.url;
+  res.json(input);
 });
 
 app.listen(process.env.PORT, function () {
