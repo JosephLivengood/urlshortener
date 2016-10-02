2d9f64967c977d72f1a79ff257efb59fb113db7d
Joseph Livengood <joeylivengood@gmail.com>
2016-10-02 15:40:48 +0000

    Loading/Redirection impli.

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 
 var express = require('express');
 var app = express();
-var path = require("path");
 var mongo = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/urlshortener';
+var ObjectId = require('mongodb').ObjectID;
 
 app.get('/new/:url', function (req, res) {
   var input = req.params.url;
   var doc = {
     url: input
-  }
+  };
   mongo.connect(url, function(err, db) {
     if (err) throw err;
     var collection = db.collection('url');
@@ -20,13 +20,30 @@ app.get('/new/:url', function (req, res) {
       res.json({
         'orginalurl': doc.url,
         'shortened': 'URL/'+doc._id
-      })
+      });
       db.close;
-    })
+    });
   });
-  //res.json(input); //res.send()
 });
 
+app.get('/url/:shortenedurl', function (req, res) {
+  var input = req.params.shortenedurl;
+  mongo.connect(url, function(err, db) {
+    if (err) throw err;
+    var collection = db.collection('url');
+    collection.find({
+      _id: new ObjectId(input)
+    }).toArray(function(err,data) {
+      if (err) console.log(err);
+      console.log(data);
+      res.statusCode = 302;
+      res.setHeader("Location", data[0].url);
+      res.end();
+      db.close();
+    })
+  });
+})
+
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port ' + process.env.PORT +'!');
+  console.log('Sever listening on port ' + process.env.PORT +'!');
 });
