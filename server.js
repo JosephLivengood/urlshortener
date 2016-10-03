35ebc0c201f3523e2736f881742844e7fcab1a7d
Joseph Livengood <joeylivengood@gmail.com>
2016-10-03 08:26:33 +0000

    regex checking url

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
-//Joseph Livengood
-
+//Joseph Livengood October 2016
 var express = require('express');
 var app = express();
 var mongo = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/urlshortener';
 var ObjectId = require('mongodb').ObjectID;
 
+//works on anything. most complicated regex ever.
+function IsURL(url) {
+  var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
+      + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftpuser@
+      + "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP URL- 199.194.52.184
+      + "|" // IP/DOMAIN
+      + "([0-9a-z_!~*'()-]+\.)*" // www.
+      + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\."
+      + "[a-z]{2,6})" // first level domain
+      + "(:[0-9]{1,4})?" // :80 ?
+      + "((/?)|" // a slash isn't required if there is no file name
+      + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+   var re=new RegExp(strRegex);
+   return re.test(url);
+}
+
 app.get('/new/:url', function (req, res) {
   var input = req.params.url;
   var doc = {
     url: input
   };
-  mongo.connect(url, function(err, db) {
-    if (err) throw err;
-    var collection = db.collection('url');
-    collection.insert(doc, function(err, data) {
-      if (err) throw err;
-      console.log(JSON.stringify(doc));
-      res.json({
+  if (!IsURL(doc.url)) {
+    res.json({
         'orginalurl': doc.url,
-        'shortened': 'URL/'+doc._id
+        'shortened': 'error: invalid URL'
       });
-      db.close;
+  } else {
+    mongo.connect(url, function(err, db) {
+      if (err) throw err;
+      var collection = db.collection('url');
+      collection.insert(doc, function(err, data) {
+        if (err) throw err;
+        console.log(JSON.stringify(doc));
+        res.json({
+          'orginalurl': doc.url,
+          'shortened': 'URL/'+doc._id
+        });
+        db.close;
+      });
     });
-  });
+  }
 });
 
 app.get('/url/:shortenedurl', function (req, res) {
